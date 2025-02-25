42c3352247e8bac86b14f8a89c84f6552fe44814
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2025-02-26 00:18:17 +0800

    zip contents of misc/Shikanoko

--- BlahajCTF_2024/misc/Shikanoko/dist/README.md
+++
@@ -1,3 +0,0 @@
-# Shikanoko but it's a Markov chain
-
-This was forked from https://github.com/nyraa/shikanoko-markov-chain
--- BlahajCTF_2024/misc/Shikanoko/dist/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-//   basePath: "/shikanoko-markov-chain",
-  reactStrictMode: false,
-};
-
-export default nextConfig;
