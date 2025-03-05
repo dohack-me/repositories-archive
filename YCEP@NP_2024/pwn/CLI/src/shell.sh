3c0c5900d7fe6b6d57c6cc4be53ea1198e7dd3b2
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2025-03-05 18:54:48 +0800

    add pwn challenges to ycep@np 2024

---
+++ YCEP@NP_2024/pwn/CLI/src/shell.sh
@@ -0,0 +1,10 @@
+echo "GREETINGS CHALLENGER! NAVIGATE THE OS TO FIND THE FLAG :)"
+echo "NOTE: After you find the flag, please move on to other challenges"
+echo "If you're caught doing funny stuff to this challenge you might get DQ-ed"
+echo ""
+
+while true; do
+  echo -n "$ "
+  read cmd
+  eval "$cmd"
+done
